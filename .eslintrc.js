a74625aad5694549a8afd262934aba6c3197cadc
Nilomiranda <nilomiranda3@gmail.com>
2021-03-25 17:37:58 -0300

    setting up typescript and lint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: false,
+    node: true,
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
