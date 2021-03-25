13fd9eadcd98cf96a16c2979866be3c0d5c23e79
Nilomiranda <nilomiranda3@gmail.com>
2021-03-25 17:56:06 -0300

    initializing discord client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hi')
+import * as Discord from 'discord.js'
+
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client?.user?.tag}!`)
+})
+
+client.on('message', msg => {
+  msg.reply(msg?.content)
+})
+
+client.login()
