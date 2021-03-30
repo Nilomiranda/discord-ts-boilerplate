4cafb812e1743bfcd1c060ddb8d2a1261f8d361e
Nilomiranda <nilomiranda3@gmail.com>
2021-03-30 17:59:08 -0300

    Added footer to help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,4 +1,5 @@
 import { Message, MessageEmbed } from 'discord.js'
+import { DISCORD_BOT_IMAGE_URL } from '../common/constants'
 
 /**
  * Add commands to be listed here, with helpful description
@@ -10,6 +11,7 @@ const createEmbededMessage = () => {
     .setColor(0x4a6fc3)
     .addField(`\`\`\`!snk load SHOP_NICE_KICKS_PRODUCT_LINKS\`\`\``, `Pass a link or list of shop nice kicks links to load into the bot`)
     .addField(`\`\`\`!sp load SHOE_PALACE_PRODUCT_LINKS\`\`\``, `Pass a link or list of shoe palace links to load into the bot`)
+    .setFooter('Calicos LOAD', DISCORD_BOT_IMAGE_URL)
 }
 
 export const helpCommand = async (message: Message): Promise<Message> => {
