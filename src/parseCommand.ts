7814b882d12cb8aa541eb789c6790d55675e0dda
Nilomiranda <nilomiranda3@gmail.com>
2021-03-29 22:41:20 -0300

    small fixes while refactoring

--- src/parseCommand.ts
+++ src/parseCommand.ts
@@ -5,11 +5,7 @@ import { guardedFunctionWrapper, UserRole } from './guards/rolesGuard'
 export const parseCommand = async (message: Discord.Message): Promise<Message> => {
   const { content } = message || {}
 
-  if (!content) {
-    return message.reply('Sorry, I cannot understand that')
-  }
-
-  if (content.startsWith('!snk load') || content.startsWith('!sp load')) {
+  if (content?.startsWith('!snk load') || content?.startsWith('!sp load')) {
     const loadCommand = await guardedFunctionWrapper(message, [UserRole.SUPPORT, UserRole.MODERATORS, UserRole.ADMIN], handleLoadCommand)
     return loadCommand(message)
   }
