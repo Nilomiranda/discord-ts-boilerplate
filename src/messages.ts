a938badadcbf848df00e9653634a755c983fa98a
Nilomiranda <nilomiranda3@gmail.com>
2021-03-30 18:51:35 -0300

    finishing up

--- src/messages.ts
+++ src/messages.ts
@@ -3,5 +3,6 @@ import { Message } from 'discord.js'
 import { parseCommand } from './parseCommand'
 
 export const readMessage = async (message: Message): Promise<Message> => {
+  console.log(message?.member)
   return parseCommand(message)
 }
