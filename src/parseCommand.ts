f64111805749604d405812ed6b063033d1a0f02a
Nilomiranda <nilomiranda3@gmail.com>
2021-03-30 22:04:45 -0300

    listing links for user

--- src/parseCommand.ts
+++ src/parseCommand.ts
@@ -5,14 +5,21 @@ import { helpCommand } from './commands/help'
 import { getVariantsFromSavedLinks } from './commands/getVariantsFromSavedLinks'
 import { MarketPlaces } from './common/constants'
 import { deleteLink } from './commands/deleteLink'
+import { listLinks } from './commands/listLinks'
 
 export const parseCommand = async (message: Message): Promise<Message> => {
   const { content } = message || {}
 
+  /**
+   * HELP COMMAND
+   */
   if (content?.startsWith('!snk-bot help')) {
     return helpCommand(message)
   }
 
+  /**
+   * LOAD LINKS COMMAND
+   */
   if (content?.startsWith('!snk load') || content?.startsWith('!sp load')) {
     const loadCommand = await guardedFunctionWrapper(
       message,
@@ -22,6 +29,9 @@ export const parseCommand = async (message: Message): Promise<Message> => {
     return loadCommand(message)
   }
 
+  /**
+   * SHOW VARIANTS COMMAND
+   */
   if (content?.startsWith('!snk variants')) {
     const getVariantsCommand = await guardedFunctionWrapper(
       message,
@@ -40,6 +50,9 @@ export const parseCommand = async (message: Message): Promise<Message> => {
     return getVariantsCommand(message, MarketPlaces.SHOE_PALACE)
   }
 
+  /**
+   * DELETE LINK COMMAND
+   */
   if (content?.startsWith('!snk delete')) {
     const deleteLinkCommand = await guardedFunctionWrapper(
       message,
@@ -56,5 +69,18 @@ export const parseCommand = async (message: Message): Promise<Message> => {
       deleteLink
     )
     return deleteLinkCommand(message, MarketPlaces.SHOE_PALACE)
+  }
+
+  /**
+   * LIST LINKS COMMAND
+   */
+  if (content?.startsWith('!snk list')) {
+    const listLinksCommand = await guardedFunctionWrapper(message, [UserRole.SUPPORT, UserRole.MODERATORS, UserRole.ADMIN, UserRole.DEVS], listLinks)
+    return listLinksCommand(message, MarketPlaces.SHOP_NICE_KICKS)
+  }
+
+  if (content?.startsWith('!sp list')) {
+    const listLinksCommand = await guardedFunctionWrapper(message, [UserRole.SUPPORT, UserRole.MODERATORS, UserRole.ADMIN, UserRole.DEVS], listLinks)
+    return listLinksCommand(message, MarketPlaces.SHOE_PALACE)
   }
 }
