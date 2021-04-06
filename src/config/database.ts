3e3437ae87ac4ad592692f3189abbf0656751881
Nilomiranda <nilomiranda3@gmail.com>
2021-04-06 17:33:59 -0300

    Cleaned up boilerplate to leave only the essential barebones

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,25 +1,31 @@
-import dotenv from 'dotenv'
-import knex from 'knex'
+/**
+ * Uncomment this in case you need to use a database
+ * It's using knex query builder by default and postgres
+ * More info in http://knexjs.org/
+ */
 
-dotenv.config()
-
-const config = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-}
-
-export const client = knex({
-  client: 'postgres',
-  connection:
-    process.env.NODE_ENV === 'development'
-      ? config
-      : {
-          connectionString: process.env.DATABASE_URL,
-          ssl: {
-            rejectUnauthorized: false,
-          },
-        },
-})
+// import dotenv from 'dotenv'
+// import knex from 'knex'
+//
+// dotenv.config()
+//
+// const config = {
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: Number(process.env.DB_PORT),
+// }
+//
+// export const client = knex({
+//   client: 'postgres',
+//   connection:
+//     process.env.NODE_ENV === 'development'
+//       ? config
+//       : {
+//           connectionString: process.env.DATABASE_URL,
+//           ssl: {
+//             rejectUnauthorized: false,
+//           },
+//         },
+// })
--- src/entities/link.ts
+++
@@ -1,8 +0,0 @@
-import { MarketPlaces } from '../common/constants'
-import { BaseEntity } from './base'
-
-export interface LinkEntity extends BaseEntity {
-  user_id: string
-  link: string
-  store: MarketPlaces
-}
